d8a087c39923193aa9948885ffc57046eebbd526
nbrown725 <nbrown72504@gmail.com>
2026-01-09 16:41:35 -0500

    - Refactored code for organization - Added automatic home directory detection - Fixed `source_dir` to default to "Downloads" when there are no args

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,75 @@ use std::collections::HashMap;
 use std::fs;
 use std::path::PathBuf;
 use std::env::args;
+use std::env::var;
+
+fn move_file(source: &PathBuf, destination: &PathBuf, name_string: &String, destination_dir: &PathBuf) {
+    if !fs::exists(&destination).expect("Error: Failed checking if copy exists") {
+        fs::rename(&source, &destination).expect("Error: Failed to move file");
+        println!("Moved {} → {}", source.to_str().unwrap(), destination.to_str().unwrap());
+    } else {
+        let mut rename = true;
+        let mut i = 1;
+
+        while rename {
+
+            // Split file name and extension
+            let (old_name, old_ext) = (&name_string).rsplit_once('.').unwrap();
+            // Add `_(i)` to the end of file name
+            let new_name = format!("{}_({}).{}", old_name, i, old_ext);
+            let new_destination = &destination_dir.join(new_name);
+
+            if !fs::exists(&new_destination).expect("Error: Failed checking if copy exists") {
+                fs::rename(&source, &new_destination).expect("Error: Failed to move file");
+                println!("Moved {} → {}", source.to_str().unwrap(), new_destination.to_str().unwrap());
+                rename = false;
+            } else {
+                i += 1;
+            }
+        }
+    }
+}
+
+fn check_file(file: &fs::DirEntry, name_string: &String) -> bool {
+    if file.file_type().expect("Error: Failed to check file type").is_file() {
+
+        // Make sure there's some '.ext'
+        if (&name_string).contains(".") {
+
+            return true;
+        }
+    }
+
+    return false;
+}
+
+fn process_file(file: fs::DirEntry, ext_map: &HashMap<&str, &str>, source_dir: &PathBuf, home_dir: &PathBuf) {
+
+    let name_string = file.file_name().into_string().expect("Error: Failed to parse filename into String");
+    
+    if check_file(&file, &name_string) {
+        // Get extension type
+        let ext = (&name_string).split(".").last().expect("Error: Failed to split filename String");
+
+        // Get destination directory
+        let destination_dir = match ext_map.get(ext) {
+            Some(p) => home_dir.join(p),
+            None => return,
+        };
+
+        // Create destination directory if absent
+        if !fs::exists(&destination_dir).expect("Error: Failed checking if path exists") {
+            fs::create_dir_all(&destination_dir).expect("Error: Failed to create necessary directory");
+        }
+
+        // Get destination and source paths
+        let destination = destination_dir.join(&name_string);
+        let source = source_dir.join(&name_string);
+
+        
+        move_file(&source, &destination, &name_string, &destination_dir);
+    }
+}
 
 fn main() {
     
@@ -24,78 +93,25 @@ fn main() {
         }
     }
 
-    let home_dir = PathBuf::from("/home/nitechno/");
-    let source_dir;
-    let user_arg = args().skip(1).next();
-    match user_arg {
-        Some(arg) => source_dir = PathBuf::from(arg),
-        None => source_dir = PathBuf::from("/home/nitechno/Downloads/"),
-    }
+    let home_dir = match var("HOME") {
+        Ok(val) => PathBuf::from(val),
+        Err(e) => {
+            println!("Error: Failed to get home directory. Stopping. {}", e);
+            return;
+        },
+    };
+
+
+    let source_dir = match args().skip(1).next() {
+        Some(arg) => PathBuf::from(arg),
+        None => PathBuf::from("Downloads"),
+    };
 
     let files = fs::read_dir(&source_dir).expect("Error: Failed to read Download directory");
 
     for f in files {
-        let file = f.expect("Error: Failed to read file");
-
-        // Make sure it's a file (not a directory)
-        if file.file_type().expect("Error: Failed to check file type").is_file() {
-
-            // Normalize file name to lowercase
-            let name_string = file.file_name().into_string().expect("Error: Failed to parse filename into String");
-
-            // Make sure there's some '.ext'
-            if (&name_string).contains(".") {
-
-                // Ext after last '.'
-                let ext = (&name_string).split(".").last().expect("Error: Failed to split filename String");
-
-                // Get path for relative ext destination
-                let ext_dir = ext_map.get(ext);
-                // Get path for absolute ext destination
-                let destination_dir;
-                match ext_dir {
-                    Some(p) => destination_dir = home_dir.join(p),
-                    None => continue,
-                }
-
-                // If path does not exist, create new directory
-                if !fs::exists(&destination_dir).expect("Error: Failed checking if path exists") {
-                    fs::create_dir_all(&destination_dir).expect("Error: Failed to create necessary directory");
-                }
-
-                // Get destination path, including filename
-                let destination = destination_dir.join(&name_string);
-                let source = source_dir.join(&name_string);
-
-                // If the file does NOT exist in destination path, move it
-                if !fs::exists(&destination).expect("Error: Failed checking if copy exists") {
-                    fs::rename(&source, &destination).expect("Error: Failed to move file");
-                    println!("Moved {} → {}", source.to_str().unwrap(), destination.to_str().unwrap());
-                } else {
-
-
-                    let mut rename = true;
-                    let mut i = 1;
-
-                    while rename {
-
-                        // Split file name and extension
-                        let (old_name, old_ext) = (&name_string).rsplit_once('.').unwrap();
-                        // Add `_(i)` to the end of file name
-                        let new_name = format!("{}_({}).{}", old_name, i, old_ext);
-                        let new_destination = &destination_dir.join(new_name);
-
-                        println!("{:?}", new_destination);
-                        if !fs::exists(&new_destination).expect("Error: Failed checking if copy exists") {
-                            fs::rename(&source, &new_destination).expect("Error: Failed to move file");
-                            println!("Moved {} → {}", source.to_str().unwrap(), new_destination.to_str().unwrap());
-                            rename = false;
-                        } else {
-                            i += 1;
-                        }
-                    }
-                }
-            }
+        if let Ok(file) = f {
+            process_file(file, &ext_map, &source_dir, &home_dir);
         }
     }
 }
